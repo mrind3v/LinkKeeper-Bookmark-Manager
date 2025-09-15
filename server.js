8d5080b6eac136af46ff66252c2a27162585d974
mrind3v <mrinmaydev@outlook.comm>
2025-09-15 06:58:51 +0000

    modified cors since we are not building frontend now and commented out logic to handle promise rejections for development purposes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
@@ -42,14 +42,14 @@ app.get('/api/health', (req, res) => {
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.error(`Error: ${err.message}`);
-  // Close server & exit process
-  server.close(() => process.exit(1));
-});
+// // Handle unhandled promise rejections
+// process.on('unhandledRejection', (err, promise) => {
+//   console.error(`Error: ${err.message}`);
+//   // Close server & exit process
+//   server.close(() => process.exit(1));
+// });
